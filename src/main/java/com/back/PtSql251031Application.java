1f44c88fb6ef15c76ed2ef53214f22d1d9938fe1
Yoepee <kdy2975@naver.com>
2025-10-31 10:12:38 +0900

    init

---
+++ src/main/java/com/back/PtSql251031Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtSql251031Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtSql251031Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/PtSql251031ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtSql251031ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
